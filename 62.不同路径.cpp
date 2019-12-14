83f3081407a3bf3afd313c6e810e5136f973860b
zhuanglewei <zhuanglewei@gmail.com>
2019-12-14 09:42:00 +0800

    添加一道题目，尚未很好解决

---
+++ 62.不同路径.cpp
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=cpp
+ *
+ * [62] 不同路径
+ *
+ * https://leetcode-cn.com/problems/unique-paths/description/
+ *
+ * algorithms
+ * Medium (57.66%)
+ * Likes:    364
+ * Dislikes: 0
+ * Total Accepted:    57K
+ * Total Submissions: 98.8K
+ * Testcase Example:  '3\n2'
+ *
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * 
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * 
+ * 问总共有多少条不同的路径？
+ * 
+ * 
+ * 
+ * 例如，上图是一个7 x 3 的网格。有多少可能的路径？
+ * 
+ * 说明：m 和 n 的值均不超过 100。
+ * 
+ * 示例 1:
+ * 
+ * 输入: m = 3, n = 2
+ * 输出: 3
+ * 解释:
+ * 从左上角开始，总共有 3 条路径可以到达右下角。
+ * 1. 向右 -> 向右 -> 向下
+ * 2. 向右 -> 向下 -> 向右
+ * 3. 向下 -> 向右 -> 向右
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: m = 7, n = 3
+ * 输出: 28
+ * 
+ */
+
+// @lc code=start
+/*
+思路：
+    第一个先做递归，虽然觉得会超时
+    然后果然超时
+        int count(int m, int n){
+        if(m==1 || n==1)
+        {
+            return 1;
+        }
+        int left = count(m-1,n);
+        int right = count(m,n-1);
+        return left+right;
+    }
+    第二个将递归改为非递归的
+        vector<vector<int>> dp{m,vector<int>(n,0)};       
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 1;
+                else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];   
+*/
+#include <vector>
+using std::vector;
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+
+    }
+
+
+};
+// @lc code=end
+
