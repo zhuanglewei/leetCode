c71d62c11d5852bc1e2d64a37425f939cb09580e
zhuanglewei <zhuanglewei@gmail.com>
2020-03-09 21:10:03 +0800

    modify a little

--- 75.颜色分类.cpp
+++ 75.颜色分类.cpp
@@ -3,12 +3,20 @@
  *
  * [75] 颜色分类
  */
-
+#include <vector>
+using std::vector;
 // @lc code=start
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-
+        if(nums.size() <= 1){
+           return;
+        }
+        for(int i: nums)
+        {
+                
+        }
+        
     }
 };
 // @lc code=end
