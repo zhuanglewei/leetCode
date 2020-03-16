e73a002bf0b22b772c824bef4ebb957cb7bb7e4c
zhuanglewei <zhuanglewei@gmail.com>
2020-03-16 20:26:40 +0800

    add a file

---
+++ 376.摆动序列.cpp
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=376 lang=cpp
+ *
+ * [376] 摆动序列
+ */
+
+// @lc code=start
+#include<vector>
+using std::vector;
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        if(nums.size() <= 1)
+        {
+            return nums.size();
+        }
+        int res = 1;
+        int state = 0;
+        int lastCount = nums[0];
+        for(int i=1; i<nums.size(); i++)
+        {
+            switch (state)
+            {
+            case 0: 
+                if(nums[i] == lastCount)
+                {
+                    break;  
+                }   
+                else{
+                    lastCount = nums[i];
+                    state = nums[i] > lastCount ? 1 : -1;
+                    res++;
+                }
+                break;
+            case 1:
+                if(nums[i] < lastCount)
+                {
+                    res++;
+                    state = -1;
+                    lastCount = nums[i];
+                }
+                break;
+            case -1:
+                if(nums[i] > lastCount) 
+                {
+                    res++;
+                    state = 1;
+                    lastCount = nums[i];
+                }
+            default:
+                break;
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
