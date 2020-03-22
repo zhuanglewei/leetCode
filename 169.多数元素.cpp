3652973d455bd03af4d29288b47f775d685de06e
zhuanglewei <zhuanglewei@gmail.com>
2020-03-23 00:01:01 +0800

    a little change

--- 169.多数元素.cpp
+++ 169.多数元素.cpp
@@ -8,6 +8,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        // 摩尔投票法
         int res=0,cnt=0;
         for(int i=0;i<nums.size();i++){
             if(cnt==0) {
