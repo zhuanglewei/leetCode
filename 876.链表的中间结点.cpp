1dec88db5f7a3016475dbd5c6461ec7df326819b
zhuanglewei <zhuanglewei@gmail.com>
2020-03-25 13:00:21 +0800

    add the explanation

--- 876.链表的中间结点.cpp
+++ 876.链表的中间结点.cpp
@@ -13,6 +13,16 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// 解题思路：
+// 双指针法：一个指针指向结果，一个指针指向末尾，但后一个指针移动两位时，前一个指针移动一位
+// 注意的点：链表个数为1时  链表个数为偶数时
+
+// Problem solving ideas:
+// Double pointer method: one pointer points to the result, one pointer points to the end, 
+//                        but when the next pointer moves two places, 
+//                        the previous pointer moves one place
+// Note: when the number of linked lists is even
+
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
