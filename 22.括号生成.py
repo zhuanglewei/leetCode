a05a46213afb41bb7966e1d2fefd62edf13f0c7d
zhuanglewei <zhuanglewei@gmail.com>
2021-08-29 12:44:14 +0800

    每日一题

---
+++ 22.括号生成.py
@@ -0,0 +1,65 @@
+#
+# @lc app=leetcode.cn id=22 lang=python3
+#
+# [22] 括号生成
+#
+# https://leetcode-cn.com/problems/generate-parentheses/description/
+#
+# algorithms
+# Medium (77.24%)
+# Likes:    1994
+# Dislikes: 0
+# Total Accepted:    332.4K
+# Total Submissions: 430.3K
+# Testcase Example:  '3'
+#
+# 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+# 
+# 有效括号组合需满足：左括号必须以正确的顺序闭合。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：n = 3
+# 输出：["((()))","(()())","(())()","()(())","()()()"]
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：n = 1
+# 输出：["()"]
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 <= n <= 8
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        def backtrace(S, left, right):
+            if len(S) == 2 * n:
+                ans.append(''.join(S))
+                return
+            if left < n:
+                S.append('(')
+                backtrace(S, left + 1, right)
+                S.pop()
+            if right < left:
+                S.append(')')
+                backtrace(S, left, right+1)
+                S.pop()
+        backtrace([], 0, 0)
+        return ans
+# @lc code=end
+
